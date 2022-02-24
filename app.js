fca20e14debdae82214406a2510b19807d5e4053
Moshe Weiner <moshesweiner@gmail.com>
2022-02-24 18:16:33 +0200

    add api

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require("express")
+const https = require("https")
 
 const app = express()
 
-app.get('/',function(req,res){
-    res.send("server is running")
+app.get('/', function (req, res) {
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=9736210d987027306218363edfd5e7c5&q=cleveland,US&units=metric"
+    
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const = weatherData JSON.parse(data)
+        })
+
+    })
+
+
+    res.send("server is up and running babay!")
+
 })
 
 
 
 
-
-
-app.listen(3000,function(){
-    console.log("server is running on port 3000");
+app.listen(3000, function () {
+    console.log("server is running on port 3000.")
 })
